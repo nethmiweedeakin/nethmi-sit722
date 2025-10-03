3696c19265541939cc53ae0ed88845c5f8ca7103
nethmiweedeakin <s224350062@deakin.edu.au>
2025-10-03 11:16:41 +1000

    Fixed injection

--- frontend/main.js
+++ frontend/main.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     // API endpoints for the Product, Order, and Customer services.
     // These ports are mapped from the Docker containers to the host machine in docker-compose.yml.
     const PRODUCT_API_BASE_URL = 'http://product-service-w10:8000';
-    const ORDER_API_BASE_URL = 'http://order-service-w10:8000';
-    const CUSTOMER_API_BASE_URL = 'http://customer-service-w10:8000';
+    const ORDER_API_BASE_URL = 'http://order-service-w10:8001';
+    const CUSTOMER_API_BASE_URL = 'http://customer-service-w10:8002';
 
     // DOM Elements
     const messageBox = document.getElementById('message-box');
